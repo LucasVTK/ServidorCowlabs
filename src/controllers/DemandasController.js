1bb1dee6222e65a775aae7f26a34c82ddfd13c00
wesleybalb <wesleybalb@gmail.comm>
2025-09-21 12:33:04 -0300

    ajustes

--- src/controllers/DemandasController.js
+++ src/controllers/DemandasController.js
@@ -1,26 +1,33 @@
-import Demandas from '../repositories/DemandasRepository.js';
+import Demandas from "../repositories/DemandasRepository.js";
 
 const demandasController = {
+    async readAll (req, res){
+        const registros = await RegistroRepository.readAll()
+        res.status(200).json(registros)
+        console.log(registros)
+    },
 
-    async FilterDemandas(req, res){
-        const { demanda_tag } = req.body
-        
-        let demandas
 
-        if(demanda_tag){
-            demandas = await Demandas.readByTag(demanda_tag)
-            return res.status(200).json({
-                total: demandas.length,
-                items: demandas
-            })
-        }
-        // se não mandar tag, retorna todas
-        demandas = await Demandas.readAll();
-        return res.status(200).json({
+  async filterDemandas(req, res) {
+    const { demanda_tag } = req.body
+
+    let demandas;
+
+    if (!demanda_tag) {
+      demandas = await Demandas.readAll();
+      return res.status(200).json({
         total: demandas.length,
-        items: demandas
-        })
+        items: demandas,
+      });
+    } else {
+      // se não mandar tag, retorna todas
+      demandas = await Demandas.readByTag(demanda_tag);
+      return res.status(200).json({
+        total: demandas.length,
+        items: demandas,
+      });
     }
-}
+  },
+};
 
-export default demandasController
+export default demandasController;
--- src/routes/RegistrosRotas.js
+++ src/routes/RegistrosRotas.js
@@ -6,6 +6,6 @@ const router = Router();
 router.get('/Allregistros/', RegistroController.MostraTodosOsRegistros)
 router.post('/auth/register', RegistroController.registro)
 
-router.post("/demandas/filter", DemandasController.FilterDemandas)
+router.post("/demandas/filter/", DemandasController.filterDemandas)
 
 export default router;
