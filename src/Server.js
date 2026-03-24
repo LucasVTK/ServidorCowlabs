3352d9ea9667ca52bc1b37bb68d2a20f84a3b457
GabrielPains <gaabrieldiasp@gmail.com>
2026-03-24 20:40:38 -0300

    auterações nas funções do serve.js e connectionSQL.js

--- src/Server.js
+++ src/Server.js
@@ -5,14 +5,15 @@ import DemandasRoute from "./routes/DemandasRoute.js";
 import con from "./database/connectionSQL.js";
 
 const app = express();
-const port = process.env.PORT;
-const host = process.env.HOST;
-const pool = await con(); // declarei essa variável dnv para receber a função importada do connectionSQL
+
+const PORT = process.env.PORT || 3000;
+// const host = process.env.HOST;
+//  const pool = await con(); // declarei essa variável dnv para receber a função importada do connectionSQL
 
 app.use(globalMiddleware.cors);
 app.use(express.json()); // sem isso o req.body vem vazio
-app.use(UserRoute);
-app.use(DemandasRoute);
+app.use("/users", UserRoute);
+app.use("/demanndas",DemandasRoute);
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -21,20 +22,44 @@ app.get("/", (req, res) => {
   });
 });
 
-// Inicia servidor só após conectar ao banco par evitar erros de conexão
-con().then(() => {
-  // estamos utilizando o .then() aqui para esperara conexao com o banco e depois dar inicio ao servidor, não sei bem o pq mas sem isso não tava funcionando antes.
-  app.listen(port, () => {
-    console.log("Servidor rodando na porta 3000");
-  });
-});
+// Criei uma função nova async para controlar melhor o start, a antiga conectava ao serve 2 vezes
+// por usar await e .then 
+async function startServer() {
+  try {
+    //conecta o banco antes de rodar o servidor, evitando erro
+    await con();
+   //aqui inicia o serve na porta definida ou na nuvem
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  } catch (err) {
+    
+    console.error("Erro ao iniciar servidor:", err);
 
-//! app.get("/", async (req, res) => {
-//!   try {
-//!    const result = await pool.request().query("SELECT user_name FROM tb_user"); //fiz essa querry simples só para testar
-//!     res.json(result.recordset); // retorna os dados como JSON
-//!   } catch (err) {
-//!     console.error(err);
-//!     res.status(500).json({ erro: err.message });
-//!   }
-//! });
+  }
+}
+//executa a função para iniciar tudo
+startServer();
+
+
+
+
+
+
+// // Inicia servidor só após conectar ao banco par evitar erros de conexão
+// con().then(() => {
+//   // estamos utilizando o .then() aqui para esperara conexao com o banco e depois dar inicio ao servidor, não sei bem o pq mas sem isso não tava funcionando antes.
+//   app.listen(port, () => {
+//     console.log("Servidor rodando na porta 3000");
+//   });
+// });
+
+// //! app.get("/", async (req, res) => {
+// //!   try {
+// //!    const result = await pool.request().query("SELECT user_name FROM tb_user"); //fiz essa querry simples só para testar
+// //!     res.json(result.recordset); // retorna os dados como JSON
+// //!   } catch (err) {
+// //!     console.error(err);
+// //!     res.status(500).json({ erro: err.message });
+// //!   }
+// //! });
--- src/database/connectionSQL.js
+++ src/database/connectionSQL.js
@@ -11,24 +11,55 @@ const sqlConfig = {
   database: process.env.DB_DATABASE,
   server: process.env.DB_SERVER,
   options: {
-    encrypt: false, 
-    trustServerCertificate: true,
+    encrypt: true, 
+    trustServerCertificate: false,
   },
 }
 
-let pool; // essa variável armazena a conexao com o banco
+let pool; // essa variável global armazena a conexao com o banco
 
-let con = async function conectar() {
-  try { // o try tenta estabelecer a conexao e se não der certo o catch retorna o erro
-    pool = await sql.connect(sqlConfig);
-    console.log("Tentando conectar ao banco...");
-    return pool // o pool tem que ser retornado para que funcione lá no arquivo server, assim retornando a conexao
+// Função para conectar ao banco
+async function conectar() {
+  try {
+
+    //evita criar várias conexões
+    if (pool) {
+      return pool;
+    }
+    console.log("BD conectando...");
+   
+    pool = await mssql.connect(sqlConfig); // cria conexão
+    console.log("BD conectado ");
+
+    return pool;
+
   } catch (err) {
     console.error("Erro ao conectar:", err.message);
+    throw err; //faz o erro subir pro server.js
+
   }
 }
 
-export default con; // exportando para ser utilizado no arquivo server
+export default conectar;
+
+
+// let con = async function conectar() {
+//   try { // o try tenta estabelecer a conexao e se não der certo o catch retorna o erro
+//     pool = await sql.connect(sqlConfig);
+//     console.log("Tentando conectar ao banco...");
+//     return pool // o pool tem que ser retornado para que funcione lá no arquivo server, assim retornando a conexao
+//   } catch (err) {
+//     console.error("Erro ao conectar:", err.message);
+//   }
+// }
+
+// export default con; // exportando para ser utilizado no arquivo server
+
+
+
+
+
+
 
 //! functions abaixo era o modo antigo de acessar o banco
 //! const configSql = {
