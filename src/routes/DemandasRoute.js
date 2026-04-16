3918e32bd4148c7de509f3b1ab0c3fcf7fcfe7fa
Hyan Nagle <147831417+Nagleeee@users.noreply.github.com>
2026-04-15 21:48:13 -0300

    comentando a rota demanda_tag, temos que ajustar no banco de dados essa tabela

--- src/routes/DemandasRoute.js
+++ src/routes/DemandasRoute.js
@@ -7,7 +7,7 @@ const DemandasRouter = Router()
 
 DemandasRouter.get('/demandas', CrudDemandaController.getAllDemandas)
 DemandasRouter.get('/demandas/id/:id', middlewareDemandas.verificarIdValido,CrudDemandaController.getDemandasById)
-DemandasRouter.get('/demandas/tag/:demanda_tag', middlewareDemandas.verificarTagValida, CrudDemandaController.getDemandasByTag) // arrumar rota demanda_tag
+//DemandasRouter.get('/demandas/tag/:demanda_tag', middlewareDemandas.verificarTagValida, CrudDemandaController.getDemandasByTag) // arrumar rota demanda_tag
 DemandasRouter.post('/demandas/create', middlewareDemandas.verificarCamposdaDemanda, CrudDemandaController.creatDemandas)
 DemandasRouter.put('/demandas/update/:id', middlewareDemandas.verificarIdValido,middlewareDemandas.verificarCamposdaDemanda, middlewareDemandas.verificarTiposDeDados, middlewareDemandas.verificarComprimento, CrudDemandaController.updateDemandas)
 //DemandasRouter.delete('/demandas/delete/:id', middlewareDemandas.verificarIdValido, CrudDemandaController.deleteDemandas)
